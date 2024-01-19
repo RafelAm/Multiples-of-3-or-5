71ea9ce056bbc7ecb8cdda0c4f9cd4b64fc66299
Rafel Amengual Tomás <147522281+RafelAm@users.noreply.github.com>
2024-01-19 10:05:38 +0100

    Create M3or5.java

---
+++ M3or5.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+      assertEquals(23, new Solution().solution(10));
+    }
+    
+}
+
+
+public class Solution {
+
+  public int solution(int number) {
+    int sum = 0;
+    
+    for(int i = 0;i < number; i++){
+      if(i % 3 == 0 || i % 5 == 0){
+        sum += i;
+      }
+    }
+    
+    return sum;
+  }
+}
